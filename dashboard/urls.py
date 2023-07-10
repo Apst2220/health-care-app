c72f6bd8e8fb511abe4f0ab24493540da8e25cf0
amanpreet262002@gmail.com <supreet.kt@gmail.com>
2023-07-09 20:52:58 -0400

    saving all templates

--- dashboard/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from django.conf import settings
-from django.conf.urls.static import static
-from . import views
-
-urlpatterns = [
-    path("", views.health_dashboard_stats, name="health_dashboard_stats"),
-    path("", views.health_dashboard_map, name="health_dashboard_map")] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
